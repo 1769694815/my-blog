e942e557f4b30e41f2c79c6d5bbbc16384047a76
徐文 <xuwen@qianyinkeji.cn>
2020-08-18 16:32:12 +0800

    feat: study js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,43 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just palying around',
+    base: '/',
+    title: '徐云舒的个人网站',
+    description: '好好学习 天天向上',
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            // { text: 'Vue', link: '/vue/' },
+            // { text: '微信小程序', link: '/wechat/' },
+            { text: '重学js', link: '/js/' }
+        ],
+        sidebar: {
+            // '/vue/': [{
+            //     title: 'Vue',
+            //     collapsable: false,
+            //     children: [
+            //         { title: '基础', path: '/vue/basic' },
+            //         { title: '进阶', path: '/vue/two' }
+            //     ]
+            // }],
+            // '/wechat/': [{
+            //     title: '微信小程序',
+            //     collapsable: false,
+            //     children: [
+            //         { title: '壹', path: '/wechat/one' },
+            //         { title: '贰', path: '/wechat/two' }
+            //     ]
+            // }],
+            '/js/': [{
+                title: '重学js',
+                collapsable: false,
+                children: [
+                    '',
+                    'basis',
+                    'advance'
+                ]
+            }]
+        },
+        lastUpdated: '上次更新'
+    },
     markdown: {
         lineNumbers: false // 为每个代码块显示行号
     }
---
+++ docs/js/advance.md
@@ -0,0 +1,23 @@
+# js 高级
+
+
+## 高阶行数
+
+## 递归
+
+
+## 装饰器
+
+
+## compose
+
+## 面向对象
+
+## 必包
+
+## bind apply call
+
+## 手写Promise
+
+
+## async + await
